afcd6bb25512e13ad196a951d8b9ad0b84ec524d
liu-jia-jun <3142467441@qq.com>
2021-12-16 08:51:13 +0800

    代码生成 2021.12.16

--- guli/service/service_edu/src/test/java/CodeGenerator.java
+++ guli/service/service_edu/src/test/java/CodeGenerator.java
@@ -27,9 +27,9 @@ public class CodeGenerator {
         // 2、全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir("E:\\work\\guli_parent\\service\\service_edu" + "/src/main/java");
+        gc.setOutputDir("E:\\code\\guli\\guli\\service\\service_edu" + "/src/main/java");
 
-        gc.setAuthor("testjava");
+        gc.setAuthor("刘佳俊");
         gc.setOpen(false); //生成后是否打开资源管理器
         gc.setFileOverride(false); //重新生成时文件是否覆盖
 
@@ -55,7 +55,7 @@ public class CodeGenerator {
         PackageConfig pc = new PackageConfig();
         pc.setModuleName("eduservice"); //模块名
         //包  com.atguigu.eduservice
-        pc.setParent("com.atguigu");
+        pc.setParent("com.guili");
         //包  com.atguigu.eduservice.controller
         pc.setController("controller");
         pc.setEntity("entity");
