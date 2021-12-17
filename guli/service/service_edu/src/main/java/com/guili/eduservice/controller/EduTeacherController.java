44cc0f1fed4df33f16e7944fcf91b4fc4099b3a0
liu-jia-jun <3142467441@qq.com>
2021-12-17 16:16:32 +0800

    update guli

---
+++ guli/service/service_edu/src/main/java/com/guili/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.guili.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author 刘佳俊
+ * @since 2021-12-16
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ guli/service/service_edu/src/main/java/com/guili/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.guili.eduservice.mapper;
+
+import com.guili.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author 刘佳俊
+ * @since 2021-12-16
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
