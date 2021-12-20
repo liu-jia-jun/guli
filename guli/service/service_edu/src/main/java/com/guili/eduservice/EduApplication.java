5c0c52e1ca3e4a051438f0a7c1a20e404f79034e
liu-jia-jun <3142467441@qq.com>
2021-12-21 00:20:18 +0800

    update

---
+++ guli/service/service_edu/src/main/java/com/guili/eduservice/EduApplication.java
@@ -0,0 +1,15 @@
+package com.guili.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 刘佳俊
+ */
+@SpringBootApplication
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+}
