9d2aa137624b5ef878a26a4b060733294b72dc13
liu-jia-jun <3142467441@qq.com>
2021-12-21 10:09:07 +0800

    update swagger

--- guli/service/service_edu/src/main/java/com/guili/eduservice/EduApplication.java
+++ guli/service/service_edu/src/main/java/com/guili/eduservice/EduApplication.java
@@ -2,11 +2,13 @@ package com.guili.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author 刘佳俊
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.service_base","com.guili"})
 public class EduApplication {
 
     public static void main(String[] args) {
--- guli/service/service_edu/src/main/java/com/guili/eduservice/controller/EduTeacherController.java
+++ guli/service/service_edu/src/main/java/com/guili/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,9 @@ package com.guili.eduservice.controller;
 
 import com.guili.eduservice.entity.EduTeacher;
 import com.guili.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,18 @@ import java.util.List;
  *
  * @author 刘佳俊
  * @since 2021-12-16
+ *
+ * Swagger
+ *
+ * @ApiOperation(“对于该方法的注释”) 作用在方法上，对方法进行注释
+ *
+ * @ApiParam(name = "id",value = "讲师ID",required = true) 作用在参数上，对参数进行约束
+ *
+ * name 对应参数的名字
+ * value 对参数进行解释
+ * required 约束该参数是否必须，是否可以为空
+ *
+ *
  */
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
@@ -28,12 +43,14 @@ public class EduTeacherController {
      * @return
      */
     @GetMapping("/findAll")
+    @ApiOperation("查询所有教师")
     public List<EduTeacher> test(){
         return eduTeacherService.list(null);
     }
 
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable("id") String id){
+    @ApiOperation("逻辑删除")
+    public boolean removeTeacher(@ApiParam(name = "id",value = "讲师ID",required = true) @PathVariable("id") String id){
         return eduTeacherService.removeById(id);
     }
 
