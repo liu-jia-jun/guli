70c981ee5571f95bd61149d342c0ea33c94f8f50
liu-jia-jun <3142467441@qq.com>
2021-12-26 21:26:28 +0800

    update

--- guli_vue/vue-admin-template-master/config/dev.env.js
+++ guli_vue/vue-admin-template-master/config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    BASE_API: '"https://localhost/5950a2419adc231f356a6636/vue-admin"',
+    // 将请求地址改为自己本地的请求地址
+    BASE_API: '"http://localhost:8000"',
 })
